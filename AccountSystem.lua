5f5da38da5ea7963d467e71760b927c39b152aaa
itzmosaa <kaigenshocks@gmail.com>
2026-01-03 01:50:50 -0500

    Update AccountSystem.lua

--- AccountSystem.lua
+++ AccountSystem.lua
@@ -3,11 +3,13 @@ return {
         {
             Username = "mosa",
             Password = "sigmarizz",
+            HWID = "ANY",
             IsActive = true
         },
         {
             Username = "karen",
             Password = "bedwars12345",
+            HWID = "ANY",
             IsActive = true
         }
     }
